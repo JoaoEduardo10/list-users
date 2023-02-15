679f813d3228103ce7ce67bbbf1fd47ef89de4b2
João Eduardo <joaoeduardoj920@gmail.com>
2023-02-15 14:14:37 -0300

    feat: add moongoose and  dataBase

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import "dotenv/config";
+import { MongoDb } from "./server/dataBase/mongoDb";
 
 import { app } from "./server/server";
 
-app.listen(8000, () => console.log(`servido iniciado`));
+// connectado ao banco de dados
+MongoDb.connect()
+  .then(() => {
+    const PORT = process.env.PORT;
+    app.listen(PORT, () => console.log(`servido iniciado na porta ${PORT}`));
+  })
+  .catch((error) => console.error(`error: ${error}`));
